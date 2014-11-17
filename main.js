842048e4e7afbc92e60d841a1583c896e543dc0e
frostiee22 <mkallo42@gmail.com>
2014-11-17 07:15:21 -0400

    remove from db if after 5 minutes if hashtag count  is less than a #

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
 var express = require('express'),
-	app 	= express(),
-	http 	= require('http').Server(app),
-	io 		= require('socket.io')(http),
-	Twitter = require('twit'),
-	config 	= require('./config.json'),
-	twitter = new Twitter(config),
-	port 	= 3000;
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    Twitter = require('twit'),
+    config = require('./config.json'),
+    twitter = new Twitter(config),
+    port = 3000;
 
 
 
-http.listen(port, function(){
-	console.log("Listening on http://127.0.0.1:"+port);
+http.listen(port, function () {
+    console.log("Listening on http://127.0.0.1:" + port);
 });
 
 app.use(express.static(__dirname + '/public'));
 
 // https://github.com/ttezel/twit
-var stream = twitter.stream('statuses/sample', {language: 'en'});
+var stream = twitter.stream('statuses/sample', {
+    language: 'en'
+});
 
 //mysql file
 var mysqldb = require('./mysql');
 
-io.on('connection', function(socket){
-	console.log('User connected ... Starting Stream connection');
+io.on('connection', function (socket) {
+    console.log('User connected ... Starting Stream connection');
 
-	//In order to minimise API usage, we only start stream from twitter when user connected
-	stream.on('tweet', function(tweet){
-		//When Stream is received from twitter
-			io.emit('new tweet' ,tweet); //Send to client via a push
-			mysqldb.CountHashTags(tweet);
-	});
+    //In order to minimise API usage, we only start stream from twitter when user connected
+    stream.on('tweet', function (tweet) {
+        //When Stream is received from twitter
+        io.emit('new tweet', tweet); //Send to client via a push
+        mysqldb.CountHashTags(tweet);
+    });
 
-	// socket.on('insert',function(insert){
-	// 	io.emit('new insert',mysqldb.populateEntities(insert));
-	// });
+    // socket.on('insert',function(insert){
+    // 	io.emit('new insert',mysqldb.populateEntities(insert));
+    // });
 
-	socket.on('disconnect', function(){
-		console.log("User disconnected");
-		stream.stop();
-	});
+    socket.on('disconnect', function () {
+        console.log("User disconnected");
+        stream.stop();
+    });
 });
 
 
 
 
 
-function setUpListeners(socket){
+function setUpListeners(socket) {
 
 }
-
-
