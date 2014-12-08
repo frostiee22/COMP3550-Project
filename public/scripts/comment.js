6deecb17335e84320d1e3e3199f3be44363e4f14
frostiee22 <mkalloo42@gmail.com>
2014-12-08 23:02:40 +0000

    small change

--- public/scripts/comment.js
+++ public/scripts/comment.js
@@ -1,47 +1,51 @@
-(function (window) {
-        "use strict";
-        $(document).ready(function () {
+(function(window) {
+    "use strict";
+    $(document).ready(function() {
 
-            defineButtons();
+        defineButtons();
 
-            setInterval(function(){
-                loadAllComments(function (data) {
-                populateTable(data);
-                });
-	       },5000);
-
+        loadAllComments(function(data) {
+            populateTable(data);
         });
 
-
-
-
-        function loadAllComments(callback) {
-            $.get("/api/comments", function (data) {
-                callback(data);
+        setInterval(function() {
+            loadAllComments(function(data) {
+                populateTable(data);
             });
-        }
+        }, 5000);
+
+    });
 
 
-        function populateTable(users, tableid, tablebodyid) {
-            var tbId = tablebodyid || "commentsList",
-                $tBody = $("#" + tbId);
-                $tBody.empty();
-                
-               users.reverse();
-            users.forEach(function (el){
-                var rec = transformRec(el);
-                $tBody.append(generateRowHTML(rec));
-            });
 
-        }
 
-        function transformRec(rec) {
-            var nRec = {};
-            nRec.name = rec.name;
-            nRec.comment = rec.comment;
-            nRec.time = rec.timestamp;
-            return nRec;
-        }
+    function loadAllComments(callback) {
+        $.get("/api/comments", function(data) {
+            callback(data);
+        });
+    }
+
+
+    function populateTable(users, tableid, tablebodyid) {
+        var tbId = tablebodyid || "commentsList",
+            $tBody = $("#" + tbId);
+        $tBody.empty();
+
+        users.reverse();
+        users.forEach(function(el) {
+            var rec = transformRec(el);
+            $tBody.append(generateRowHTML(rec));
+        });
+
+    }
+
+    function transformRec(rec) {
+        var nRec = {};
+        nRec.name = rec.name;
+        nRec.comment = rec.comment;
+        nRec.time = rec.timestamp;
+        return nRec;
+    }
 
 
 
@@ -49,10 +53,10 @@
     function generateRowHTML(rec) {
         var str = '<li class="commentList">';
         str += '<div class="commenterImage">';
-        str += '<img src = "http://americanmuslimconsumer.com/wp-content/uploads/2013/09/blank-user.jpg"/>';
+        str += '<img src = "../images/blank-user.jpg"/>';
         str += '</div>';
         str += '<div class="commentText">';
-        str += '<p class=""><h4>'+ rec.name +'</h4>'+ rec.comment +'</p><span class="date sub-text">'+rec.time+'</span>';
+        str += '<p class=""><h4>' + rec.name + '</h4>' + rec.comment + '</p><span class="date sub-text">' + rec.time + '</span>';
         str += '</div>';
         str += ' </li>';
         return str;
@@ -60,9 +64,9 @@
 
 
 
-     function defineButtons() {
+    function defineButtons() {
         //configure buttons by assigning functionality programatically
-        $("#submitid").click(function () {
+        $("#submitid").click(function() {
             console.log("sub Button Clicked");
 
         });
