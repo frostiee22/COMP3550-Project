b6f9d67a888a3ffecb103ec45d546c9228194160
frostiee22 <mkallo42@gmail.com>
2014-12-16 17:06:01 -0400

    small changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,28 @@
 (function (window) {
     "use strict";
     $(document).ready(function () {
-        var socket = io.connect();
+        var socket = io.connect(),
+            check = 0;
 
-        socket.on('new tweet', function (tweet) {
+        // incomming twitter stream form the server
+            socket.on('new tweet', function (tweet) {
+                if (check == 0) {
+                    //console.log(tweet);
+                    check = 1;
+                }
+            });
 
-            // incomming twitter stream form the server
+
+        setInterval(function () {
+            check = 0;
+        }, 5000);
 
 
-        });
 
-        loadAllRecs(function (data) {
+
+        loadAllHashTags(function (data) {
             var tbH = "tblHead",
-            $tHead = $("#" + tbH);
+                $tHead = $("#" + tbH);
             $tHead.append("<tr><th>tags</th><th>count</th></tr>");
             populateTable(data);
         });
@@ -38,20 +48,30 @@
         });
 
         $tbl.dataTable();
+
+
+
     }
 
 
     function transformRec(rec) {
         var nRec = {};
-        nRec.tag = rec.tag;
-        nRec.count = rec.times;
-        return nRec;
+        if (rec.tag != undefined) {
+            nRec.name = rec.tag;
+            nRec.count = rec.times;
+            return nRec;
+        } else {
+            nRec.name = rec.location;
+            nRec.count = rec.tweets;
+            return nRec;
+        }
+
     }
 
 
     function generateRowHTML(rec) {
         var str = "<tr>";
-        str += "<td>" + rec.tag + "</td>";
+        str += "<td>" + rec.name + "</td>";
         str += "<td>" + rec.count + "</td>";
         str += "</tr>";
         return str;
@@ -69,15 +89,14 @@
     }
 
 
-    function loadAllRecs(callback) {
+    function loadAllHashTags(callback) {
         $.get("/api/hashtags", function (data) {
             callback(data);
         });
     }
 
-    function loadTop15(callback) {
-        $.get("/api/hashtags/top15tags", function (data) {
-            console.log(data.length);
+    function LoadAllLocations(callback) {
+        $.get("/api/location/tweets/0/150", function (data) {
             callback(data);
         });
     }
@@ -89,28 +108,33 @@
         //configure buttons by assigning functionality programatically
         $("#btnAll").click(function () {
             console.log("All Button Clicked");
-            loadAllRecs(function (data) {
-                removeTable(); // Remove records before adding new records
+            $("#info").empty();
+            $("#info").append("Showing the amount of times a #tag was used");
+
+            // Remove records before adding new records
+            removeTable();
+            loadAllHashTags(function (data) {
 
                 var tbH = "tblHead",
                     $tHead = $("#" + tbH);
-
-            $tHead.append("<tr><th>tags</th><th>count</th></tr>");
-                populateTable(data);
-            });
-        });
-        $("#btnTop15").click(function () {
-            console.log("top15 Button Clicked");
-            loadTop15(function (data) {
-                removeTable(); // Remove records before adding new records
-
-                var tbH = "tblHead",
-                $tHead = $("#" + tbH);
-
                 $tHead.append("<tr><th>tags</th><th>count</th></tr>");
                 populateTable(data);
             });
+        });
 
+        $("#btnTop15").click(function () {
+            console.log("top15 Button Clicked");
+            $("#info").empty();
+            $("#info").append("Showing amount of tweets in a location");
+
+            // Remove records before adding new records
+            removeTable();
+            LoadAllLocations(function (data) {
+                var tbH = "tblHead",
+                    $tHead = $("#" + tbH);
+                $tHead.append("<tr><th>locations</th><th>count</th></tr>");
+                populateTable(data);
+            });
         });
     }
 
